23559c2dcee9aed2b4ae43a0896fc89584f7f2c8
Ernest Haruna <ernestharuna1@gmail.com>
2022-10-19 16:50:49 +0100

    Added mobile nav

---
+++ app.js
@@ -0,0 +1,7 @@
+
+function openNav() {
+   document.getElementsByClassName('lower-nav')[0].style.left = "0";
+}
+function closeNav() {
+   document.getElementsByClassName('lower-nav')[0].style.left = "-900px";
+}
