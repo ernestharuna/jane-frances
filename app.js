67c059db95502ae8cdc994135a6358cdb36c7b6f
Ernest Haruna <ernestharuna1@gmail.com>
2022-10-22 21:58:14 +0100

    scroll to top button, and nav

--- app.js
+++ app.js
@@ -1,7 +1,34 @@
 
+// for top nav
 function openNav() {
-   document.getElementsByClassName('lower-nav')[0].style.left = "0";
+   document.getElementsByClassName('side-nav')[0].style.left = "0";
 }
 function closeNav() {
-   document.getElementsByClassName('lower-nav')[0].style.left = "-900px";
+   document.getElementsByClassName('side-nav')[0].style.left = "-900px";
+}
+
+// for fixed scroll to top button
+window.onload = function () {
+   const shownOnPx = 900;
+   const btn = document.querySelector(".to-top");
+
+   const scrollFunc = () => {
+      return document.documentElement || document.body;
+   }
+
+   document.addEventListener("scroll", () => {
+      if (scrollFunc().scrollTop > shownOnPx) {
+         btn.classList.remove("hidden")
+      } else {
+         btn.classList.add("hidden")
+      }
+   })
+
+   const toTop = () => {
+      document.body.scrollIntoView({
+         behavior: "smooth",
+      });
+   }
+
+   btn.addEventListener("click", toTop)
 }
